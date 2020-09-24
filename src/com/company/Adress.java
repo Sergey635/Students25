5882604881ca86b4ea40b64d3a08dd12b605cf86
java01 <sergo231993@gmail.com>
2020-09-24 20:29:28 +0300

    Initial commit

---
+++ src/com/company/Adress.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.time.LocalDateTime;
+
+public class Adress {
+    private String country;
+    private String district;
+    private String city;
+    private String street;
+    private String building;
+    private String apartment;
+    private String currentAdress;
+    private LocalDateTime created_at;
+    private LocalDateTime modified_at;
+
+}
---
+++ src/com/company/Student.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.List;
+
+public class Student {
+    private  String id;
+    private  String firstName;
+    private  String lastName;
+    private  String patronymic;
+    private boolean gender;
+    private String phoneNumber;
+    private String country;
+    private String district;
+    private String city;
+    private String street;
+    private String building;
+    private String apartment;
+    private String contactPersonPhone;
+    private String university;
+    private String faculty;
+    private String chair;
+    private String group;
+    private String yearOfEntry;
+    private boolean grant;
+    private String grantNotes;
+    private boolean reservist;
+    private String beneficiaryNote;
+    private String curator;
+    private List<String> sport;
+    private String currentAdress;
+
+}
+
